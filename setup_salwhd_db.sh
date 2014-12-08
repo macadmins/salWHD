db337272ab2ca5e673a98c50f3e2f73a6bb2cbf9
Nick McSpadden <nmcspadden@gmail.com>
2014-12-08 10:14:01 -0800

    Setup script now uses correct db

--- setup_salwhd_db.sh
+++ setup_salwhd_db.sh
@@ -6,6 +6,6 @@ DB_PASS=password
 echo "CREATE TABLE casperimport(id INT PRIMARY KEY NOT NULL, serial TEXT, name TEXT, model TEXT, ios_version TEXT, ipaddress TEXT, macaddress TEXT, bluetooth TEXT, capacity TEXT, username TEXT, email TEXT, asset_tag TEXT);" | docker run \
   --rm \
   --interactive \
-  --link postgres-sal:postgres \
+  --link postgres-whd:postgres \
   postgres:latest \
   bash -c 'exec psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres'
